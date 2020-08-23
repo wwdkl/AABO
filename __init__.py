21698acf44d7f06e20f46503964c6d822cf18f71
wwdkl <wwdkl20@outlook.com>
2020-08-23 23:57:03 +0800

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,20 @@
+#mmdetection/mmdet/models/anchor_heads/__init__.py
+from .anchor_head import AnchorHead
+from .fcos_head import FCOSHead
+from .fovea_head import FoveaHead
+from .free_anchor_retina_head import FreeAnchorRetinaHead
+from .ga_retina_head import GARetinaHead
+from .ga_rpn_head import GARPNHead
+from .guided_anchor_head import FeatureAdaption, GuidedAnchorHead
+from .reppoints_head import RepPointsHead
+from .retina_head import RetinaHead
+from .retina_sepbn_head import RetinaSepBNHead
+from .rpn_head import RPNHead
+from .ssd_head import SSDHead
+from .aabo_rpn_head import AABORPNHead
+
+__all__ = [
+    'AnchorHead', 'GuidedAnchorHead', 'FeatureAdaption', 'RPNHead','BOSSRPNHead',
+    'GARPNHead', 'RetinaHead', 'RetinaSepBNHead', 'GARetinaHead', 'SSDHead',
+    'FCOSHead', 'RepPointsHead', 'FoveaHead', 'FreeAnchorRetinaHead'
+]
